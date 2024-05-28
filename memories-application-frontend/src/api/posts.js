f62c9e3359f833fc24eedf43cd0548f56cca5fc1
elisei-florian-catalin <catalinelisei2002@gmail.com>
2024-05-28 16:40:35 +0300

    fix backend url

--- memories-application-frontend/src/api/posts.js
+++ memories-application-frontend/src/api/posts.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = "https://memories-application-backend.vercel.app/";
+const url = "https://memories-application-backend.vercel.app/posts";
 
 export const createPost = (post) => axios.post(url, post);
 export const deletePost = (id) => axios.delete(`${url}/${id}`);
--- memories-application-frontend/src/api/users.js
+++ memories-application-frontend/src/api/users.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = "https://memories-application-backend.vercel.app/";
+const url = "https://memories-application-backend.vercel.app/users";
 
 export const signIn = (formData) => axios.post(`${url}/signin`, formData);
 export const signUp = (formData) => axios.post(`${url}/signup`, formData);
